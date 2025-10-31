818b798faded01f6ae7354d710411749cbecadce
isaque14 <isaquediniz14@gmail.com>
2025-10-30 23:12:36 -0300

    Feature: Extension

---
+++ Extensions/MediatorExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Mediator.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Mediator.Extensions
+{
+    public static class MediatorExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddTransient<IMediator, Implementation.Mediator>();
+
+            var handlerType = typeof(IHandler<,>);
+
+            foreach (var assembly in assemblies)
+            {
+                var handlers = assembly.GetTypes()
+                    .Where(type => !type.IsAbstract && !type.IsInterface)
+                    .SelectMany(x => x.GetInterfaces(), (t, i) => new { Type = t, Interface = i })
+                    .Where(ti => ti.Interface.IsGenericType && ti.Interface.GetGenericTypeDefinition() == handlerType);
+
+                foreach (var handler in handlers)
+                {
+                    services.AddTransient(handler.Interface, handler.Type);
+                }
+            }
+
+            return services;
+        }
+
+        //Para add no Program.cs services.AddMediator(typeof(Program).Assembly);
+    }
+}
